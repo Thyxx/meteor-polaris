e173f977b161d377f1cbaf79e4ac5a8e18d77443
Thomas Cristina de Carvalho <thomas.c.decarvalho@iskn.co>
2017-11-06 21:55:25 +0100

    Auto stash before revert of "Add method to check if login email and password are correct"

--- imports/ui/components/login.js
+++ imports/ui/components/login.js
@@ -8,18 +8,57 @@ import {
   Button,
   Icon,
 } from '@shopify/polaris';
+import yup from 'yup';
 
 export default class Login extends Component {
   constructor() {
     super();
     this.state = {
-      email: '',
-      password: '',
+      email: {
+        value: '',
+        valid: true,
+      },
+      password: {
+        value: '',
+        valid: true,
+      },
     };
   }
 
   valueUpdater(field) {
-    return value => this.setState({ [field]: value });
+    return value => this.setState({
+      [field]: {
+        value,
+        valid: true,
+      },
+    });
+  }
+
+  fieldValidator(field) {
+    let schema;
+    if (field === 'email') {
+      schema = yup.string().email().required();
+    } else if (field === 'password') {
+      schema = yup.string().min(6);
+    }
+    schema.isValid(this.state[field].value)
+      .then((valid) => {
+        if (valid) {
+          this.setState({
+            [field]: {
+              valid: true,
+              value: this.state[field].value,
+            },
+          });
+        } else {
+          this.setState({
+            [field]: {
+              valid: false,
+              value: this.state[field].value,
+            },
+          });
+        }
+      });
   }
 
   render() {
@@ -39,18 +78,22 @@ export default class Login extends Component {
               <div style={{ width: '460px' }}></div>
               <TextField
                 prefix={<Icon color={'inkLightest'} source="circlePlus" />}
-                value={this.state.email}
+                value={this.state.email.value}
                 label="Email"
                 type="email"
                 onChange={this.valueUpdater('email')}
+                onBlur={() => this.fieldValidator('email')}
+                error={!this.state.email.valid && this.state.email.value.length > 0 ? 'Use a valid email address.' : false}
               />
               <TextField
                 prefix={<Icon color={'inkLightest'} source="search" />}
-                value={this.state.password}
+                value={this.state.password.value}
                 label="Password"
                 type="password"
                 minLength={6}
                 onChange={this.valueUpdater('password')}
+                onBlur={() => this.fieldValidator('password')}
+                error={!this.state.password.valid && this.state.password.value.length > 0 ? 'Password must contain at least 6 characters.' : false}
               />
               <Button
                 submit

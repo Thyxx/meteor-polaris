82d012d3f422d35898efbc3519d0f069d62b863e
Thomas Cristina de Carvalho <thomas.c.decarvalho@iskn.co>
2017-11-11 13:46:03 +0100

    Add disabled refresh button

--- imports/ui/pages/orders.js
+++ imports/ui/pages/orders.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {
   Page,
   Layout,
+  Icon,
 } from '@shopify/polaris';
 import OrdersListContainer from '../containers/ordersListContainer';
 
@@ -10,6 +11,7 @@ export default class Orders extends Component {
     return (
       <Page
         title="Orders"
+        primaryAction={{ content: 'Refresh', icon: 'refresh', disabled: true }}
       >
         <Layout>
           <Layout.Section>
